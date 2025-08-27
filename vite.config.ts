fe7cc2bac2b8b1a90546cfa41979d92b96cf1fbf
casonshep <cshepard@caltech.edu>
2025-08-27 11:56:10 -0700

    react deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/casonshep.github.io/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
+  base: '/casonshep-portfolio/'
 })
